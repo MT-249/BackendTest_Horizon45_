ea8b54d663834a5a2e5978b0ccef24e1f8bcfd07
MT-249 <123061155+MT-249@users.noreply.github.com>
2023-06-23 15:27:50 +0800

    Initial commit

--- truckproject/truckapp/views.py
+++ truckproject/truckapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .forms import driverForm
+from .models import Driver
+from .serializers import DriverSerializer
+from rest_framework import generics
 
 def index(request):
     return render(request, "index.html")
@@ -9,11 +12,11 @@ def contact_view(request):
     # Other view logic...
     return render(request, 'T-rucker.html', {'form': form})
 
-from rest_framework import generics
-from .models import Driver
-from .serializers import DriverSerializer
-
 class DriverListAPIView(generics.ListAPIView):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+
+class DriverRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
 
